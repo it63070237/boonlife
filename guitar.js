19eff2b6ac537ab39699d03b2505f08ba7d4dcbd
63070177 <73645471+63070177@users.noreply.github.com>
2021-10-20 14:09:31 +0700

    Add files via upload

---
+++ guitar.js
@@ -0,0 +1,8 @@
+function on() {
+    document.getElementById("goverlay").style.display = "block";
+}
+
+function off() {
+    let goverlay = document.getElementById("goverlay");
+    goverlay.style.display = "none";
+}
