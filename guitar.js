9c881882db1edde60c14afa2d1440a241d806150
63070177 <73645471+63070177@users.noreply.github.com>
2021-10-24 16:18:41 +0700

    Update guitar.js

--- guitar.js
+++ guitar.js
@@ -1,8 +1,138 @@
+
+
 function on() {
     document.getElementById("goverlay").style.display = "block";
 }
 
+function on2() {
+    document.getElementById("goverlay2").style.display = "block";
+}
+
+function on3() {
+    document.getElementById("goverlay3").style.display = "block";
+}
+
+function on4() {
+    document.getElementById("goverlay4").style.display = "block";
+}
+function on5() {
+    document.getElementById("goverlay5").style.display = "block";
+}
+function on6() {
+    document.getElementById("goverlay6").style.display = "block";
+}
+function on7() {
+    document.getElementById("goverlay7").style.display = "block";
+}
+function on8() {
+    document.getElementById("goverlay8").style.display = "block";
+}
+function on9() {
+    document.getElementById("goverlay9").style.display = "block";
+}
+function on10() {
+    document.getElementById("goverlay10").style.display = "block";
+}
+
 function off() {
-    let goverlay = document.getElementById("goverlay");
-    goverlay.style.display = "none";
+    document.getElementById("goverlay").style.display = "none";
+    document.getElementById("goverlay2").style.display = "none";
+    document.getElementById("goverlay3").style.display = "none";
+    document.getElementById("goverlay4").style.display = "none";
+    document.getElementById("goverlay5").style.display = "none";
+    document.getElementById("goverlay6").style.display = "none";
+    document.getElementById("goverlay7").style.display = "none";
+    document.getElementById("goverlay8").style.display = "none";
+    document.getElementById("goverlay9").style.display = "none";
+    document.getElementById("goverlay10").style.display = "none";
+}
+
+function next1(){
+    document.getElementById("goverlay").style.display = "none";
+    document.getElementById("goverlay2").style.display = "block";
+}
+
+function next2(){
+    document.getElementById("goverlay2").style.display = "none";
+    document.getElementById("goverlay3").style.display = "block";
+}
+
+function next3(){
+    document.getElementById("goverlay3").style.display = "none";
+    document.getElementById("goverlay4").style.display = "block";
+}
+
+function next4(){
+    document.getElementById("goverlay4").style.display = "none";
+    document.getElementById("goverlay5").style.display = "block";
+}
+
+function next5(){
+    document.getElementById("goverlay5").style.display = "none";
+    document.getElementById("goverlay6").style.display = "block";
+}
+
+function next6(){
+    document.getElementById("goverlay6").style.display = "none";
+    document.getElementById("goverlay7").style.display = "block";
+}
+
+function next7(){
+    document.getElementById("goverlay7").style.display = "none";
+    document.getElementById("goverlay8").style.display = "block";
+}
+
+function next8(){
+    document.getElementById("goverlay8").style.display = "none";
+    document.getElementById("goverlay9").style.display = "block";
+}
+
+function next9(){
+    document.getElementById("goverlay9").style.display = "none";
+    document.getElementById("goverlay10").style.display = "block";
+}
+
+function backto1(){
+    document.getElementById("goverlay2").style.display = "none";
+    document.getElementById("goverlay").style.display = "block";
+}
+
+function backto2(){
+    document.getElementById("goverlay3").style.display = "none";
+    document.getElementById("goverlay2").style.display = "block";
+}
+
+function backto3(){
+    document.getElementById("goverlay4").style.display = "none";
+    document.getElementById("goverlay3").style.display = "block";
+}
+
+function backto4(){
+    document.getElementById("goverlay5").style.display = "none";
+    document.getElementById("goverlay4").style.display = "block";
+}
+
+function backto5(){
+    document.getElementById("goverlay6").style.display = "none";
+    document.getElementById("goverlay5").style.display = "block";
+}
+
+function backto6(){
+    document.getElementById("goverlay7").style.display = "none";
+    document.getElementById("goverlay6").style.display = "block";
+}
+
+function backto7(){
+    document.getElementById("goverlay8").style.display = "none";
+    document.getElementById("goverlay7").style.display = "block";
+}
+
+function backto8(){
+    document.getElementById("goverlay9").style.display = "none";
+    document.getElementById("goverlay8").style.display = "block";
+}
+
+function backto9(){
+    document.getElementById("goverlay10").style.display = "none";
+    document.getElementById("goverlay9").style.display = "block";
 }
